7c4128efce9446f1ade24d18e37cbb3502c8b643
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-10-14 11:11:08 +0530

    Update dashboard.js
    nav position changed

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -2,29 +2,29 @@ import React, { useContext, useState } from "react";
 import styles from "./dashboard.module.scss";
 import { GetAdminContext } from "../../app/state/contexts/adminContext";
 import Home from "./components/Home/Home";
-//import Posts from "./components/Posts/Posts";
+import Posts from "./components/Posts/Posts";
 import Settings from "./components/Settings/Settings";
-//import Reports from "./components/Reports/Reports";
+import Reports from "./components/Reports/Reports";
 import PendingPayments from "./components/PendingPayments/PendingPayments";
 
 function Dashboard() {
   const [state, dispatch] = useContext(GetAdminContext);
-  const [selectedTab, setSelectedTab] = useState("Home");
+  const [selectedTab, setSelectedTab] = useState("Settings");
   console.log("state?.loggedIn", state?.loggedIn);
   console.log("state", state);
   if (state?.loggedIn) {
     return (
       <div className={styles.container}>
         <nav>
-          <a
-            onClick={() => setSelectedTab("Home")}
+              <a
+            onClick={() => setSelectedTab("Settings")}
             style={{
               borderBottom: `2px ${
-                selectedTab === "Home" ? "black" : "transparent"
+                selectedTab === "Settings" ? "black" : "transparent"
               } solid`,
             }}
           >
-            Report
+            Settings
           </a>
           <a
             onClick={() => setSelectedTab("Posts")}
@@ -54,17 +54,17 @@ function Dashboard() {
               } solid`,
             }}
           >
-            Redeem Code
+            Redeem
           </a>
-          <a
-            onClick={() => setSelectedTab("Settings")}
+                <a
+            onClick={() => setSelectedTab("Home")}
             style={{
               borderBottom: `2px ${
-                selectedTab === "Settings" ? "black" : "transparent"
+                selectedTab === "Home" ? "black" : "transparent"
               } solid`,
             }}
           >
-            Settings
+            Report
           </a>
         </nav>
         <div className={styles.tabContent}>          
