360b1e7fcfedf52bc487aa8d2db260494f5069a4
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-10-31 17:45:37 +0530

    Update Home.js
    Coin chng to 1

--- components/LoginStack/components/Home/Home.js
+++ components/LoginStack/components/Home/Home.js
@@ -234,7 +234,7 @@ function Home({ data }) {
         {/* <h3>You can start earning now 💵</h3> */}
         {/* <Link href={path}> */}
         {data.coinsGenerated !== 0 &&
-          data.coinsGenerated % 15 === 0 &&
+          data.coinsGenerated % 1 === 0 &&
           !validated && (
             <>
               <button className={styles.btn} onClick={buttonHandler}>
