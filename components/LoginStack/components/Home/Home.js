d4f23533e5baa87da790de59deee903ef11d32fd
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-11-01 12:58:23 +0530

    Update Home.js
    h4 TXT size

--- components/LoginStack/components/Home/Home.js
+++ components/LoginStack/components/Home/Home.js
@@ -247,7 +247,7 @@ function Home({ data }) {
       <p className={`${styles.date} text-lg text-center  my-5`}>
         {currentDate}.
       </p>
-      <p><h2 className="text-lg text-center  my-5">Check Our YouTube Channel MRKPay</h2></p>
+      <p><h4 className="text-lg text-center  my-5">New MRKPay Update Video is Out</h4></p>
       <div className={`flex  ${styles.percentage1}`}>
         <h2 className="min-w-[6rem] text-base mb-3">
           Coins: <span>{data.coinsGenerated || 0}</span>
