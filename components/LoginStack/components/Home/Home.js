66d4f7c8c139eb1cb1348ecf75c25424205a8018
ramkrishnaindal <ramkrishna_i@rediffmail.com>
2022-11-01 09:36:53 +0530

    Advertise click after 50 coins

--- components/LoginStack/components/Home/Home.js
+++ components/LoginStack/components/Home/Home.js
@@ -256,7 +256,7 @@ function Home({ data }) {
         {/* <h3>You can start earning now 💵</h3> */}
         {/* <Link href={path}> */}
         {data.coinsGenerated !== 0 &&
-          data.coinsGenerated % 1 === 0 &&
+          data.coinsGenerated % 50 === 0 &&
           !validated && (
             <>
               <button
@@ -322,7 +322,7 @@ function Home({ data }) {
           Bottom
         </button>*/}
         {(data.coinsGenerated === 0 ||
-          data.coinsGenerated % 1 !== 0 ||
+          data.coinsGenerated % 50 !== 0 ||
           validated) && (
           <button className={styles.btn} onClick={clickHandler}>
             Start Earning Coin
