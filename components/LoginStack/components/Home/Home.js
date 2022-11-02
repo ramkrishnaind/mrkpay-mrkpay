6960639418201cbf17f6d9883708f232f9c13f30
mrkpay <111171488+mrkpay@users.noreply.github.com>
2022-11-02 16:26:32 +0530

    Update Home.js
    rc txt

--- components/LoginStack/components/Home/Home.js
+++ components/LoginStack/components/Home/Home.js
@@ -265,6 +265,12 @@ function Home({ data }) {
         </a>
       </Link>
 </p>
+ <p className="text-lg text-center  my-5">
+        <span style={{
+          fontWeight: '600',
+          color: 'red',
+          fontSize: 18
+        }}>Withdrawal Coin Fixed 👍</span></p>
 {/*<p className="text-lg text-center  my-5"><Link  href="https://youtu.be/Bj3m_aFSHQo">https://youtu.be/Bj3m_aFSHQo</Link></p>*/}
       <div className={`flex  ${styles.percentage1}`}>
         <h2 className="min-w-[6rem] text-base mb-3">
